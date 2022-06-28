27d8a0a76b9232f4b7aab198a5655e603f0bf6c0
Abhranil Chandra <57188942+abhraIITKgp@users.noreply.github.com>
2022-06-28 12:41:05 +0530

    Add files via upload

---
+++ MARL_PistonBall_PPO/gym_test.py
@@ -0,0 +1,16 @@
+import gym
+env = gym.make("CartPole-v1")
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+
+    if done:
+        observation, info = env.reset(return_info=True)
+env.close()
+
+print('observation:')
+print(observation)
+print('info:')
+print(info)
